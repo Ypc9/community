92f98687cea2478c3193d09756482ab251c6122c
yinpengcheng <1099332640@qq.com>
2021-04-28 14:11:41 +0800

    add index and bootstrap

--- src/main/java/com/ypc/community/controller/IndexController.java
+++ src/main/java/com/ypc/community/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.ypc.community.controller;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +13,15 @@ import org.springframework.web.bind.annotation.RequestParam;
  *@创建时间 2021/4/27
  */
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model) {
-        model.addAttribute("name",name);
-        return "hello";
-    }
+//    @GetMapping("/hello")
+//    public String hello(@RequestParam(name = "name") String name, Model model) {
+//        model.addAttribute("name",name);
+//        return "index";
+//    }
+    @GetMapping("/")
+    public String index(){ return "index"; }
 
 //    public static void main(String[] args){
 //        SpringApplication.run(HelloController.class,args);
