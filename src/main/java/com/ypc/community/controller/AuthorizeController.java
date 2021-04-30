308e9f5195e392c8a4f9b882a6d3d80035016ce3
yinpengcheng <1099332640@qq.com>
2021-04-30 20:24:15 +0800

    add check cookies

--- src/main/java/com/ypc/community/controller/AuthorizeController.java
+++ src/main/java/com/ypc/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+//import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -42,7 +44,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+//                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 //        accessTokenDTO.setClient_id("Iv1.9b1dc2338293f93c");
         accessTokenDTO.setClient_id(clientId);
@@ -58,16 +61,18 @@ public class AuthorizeController {
         //注意：个人的GitHub中如果没有name和bio信息，就会报错
         if (githubUser != null) {
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());//当前时间计算
             user.setGmtModified(user.getGmtModified());
-            userMapper.insert(user);
+            userMapper.insert(user);//插入数据库的过程相当于写入session
+            response.addCookie(new Cookie("token",token));//自动写入cookie
             //登陆成功，写cookie 和session
-            request.getSession().setAttribute("user", githubUser);
+//            request.getSession().setAttribute("user", githubUser);
 //            return "redirect:index";//重定向页面  找不到地址报错
-            return "redirect:/";//  redirect:/ 意思是它会在本地再请求一次
+            return "redirect:/";//  redirect:/ 意思是它会在本地再请求一次   手动写入cookie
         } else {
             //登陆失败，重新登陆
             return "redirect:/";
--- src/main/java/com/ypc/community/controller/IndexController.java
+++ src/main/java/com/ypc/community/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.ypc.community.controller;
 
+import com.ypc.community.mapper.UserMapper;
+import com.ypc.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *@描述
@@ -15,13 +21,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
 //    @GetMapping("/hello")
 //    public String hello(@RequestParam(name = "name") String name, Model model) {
 //        model.addAttribute("name",name);
 //        return "index";
 //    }
     @GetMapping("/")
-    public String index(){ return "index"; }
+    public String index(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user !=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;//如果命中就break掉
+            }
+        }
+
+
+        return "index";
+    }
 
 //    public static void main(String[] args){
 //        SpringApplication.run(HelloController.class,args);
